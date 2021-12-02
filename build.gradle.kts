096b26e48f690f083ac3e35c5646435b483eb2fc
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-12-02 11:36:05 +0000

    chore: Kotlin bumped to 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion.VERSION_11
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.21-2"
-    kotlin("plugin.spring") version "1.4.21-2"
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.spring") version "1.6.0"
     id("java-library")
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     java
