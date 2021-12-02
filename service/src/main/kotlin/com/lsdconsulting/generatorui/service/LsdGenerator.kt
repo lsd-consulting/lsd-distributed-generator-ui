f4d4cf740af4c2ba1297c9bd5f6a2850617a9f99
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-12-02 12:49:07 +0000

    refactor: ParticipantListGenerator extracted

--- service/src/main/kotlin/com/lsdconsulting/generatorui/service/LsdGenerator.kt
+++ service/src/main/kotlin/com/lsdconsulting/generatorui/service/LsdGenerator.kt
@@ -1,10 +1,5 @@
 package com.lsdconsulting.generatorui.service
 
-import com.lsd.ParticipantType.*
-import com.lsd.events.Message
-import com.lsd.events.SequenceEvent
-import com.lsd.events.SynchronousResponse
-import com.lsd.report.model.Participant
 import io.lsdconsulting.lsd.distributed.generator.diagram.InteractionGenerator
 import org.springframework.stereotype.Service
 import java.util.*
@@ -13,51 +8,16 @@ import java.util.*
 class LsdGenerator(
     private val interactionGenerator: InteractionGenerator,
     private val htmlReportRenderer: HtmlReportRenderer,
-    private val scenarioBuilder: ScenarioBuilder
+    private val scenarioBuilder: ScenarioBuilder,
+    private val participantListGenerator: ParticipantListGenerator
 ) {
     fun captureInteractionsFromDatabase(vararg traceIds: String): String {
         val traceIdToColourMap: HashMap<String, Optional<String>> = HashMap()
         traceIds.forEach { x: String -> traceIdToColourMap[x] = Optional.empty() }
         val events = interactionGenerator.generate(traceIdToColourMap)
-        val participants = generateParticipants(events)
+        val participants = participantListGenerator.generateParticipants(events)
         val title = "Diagram for traceIds: ${traceIds.asList().joinToString()}"
         val scenario = scenarioBuilder.build(title = title, events = events, traceIds = traceIds.asList(), participants = participants.toList())
         return htmlReportRenderer.render(scenario)
-    }
-
-    private fun generateParticipants(events: MutableList<SequenceEvent>): LinkedHashSet<Participant> {
-        val participantNames = mutableSetOf<String>()
-        val participants = linkedSetOf<Participant>()
-        events.forEach {
-            if (it is Message && it !is SynchronousResponse) {
-                when (it.label) {
-                    "publish event" -> {
-                        addToParticipants(participantNames, it.from, participants, PARTICIPANT.called(it.from))
-                        addToParticipants(participantNames, it.to, participants, ENTITY.called(it.to))
-                    }
-                    "consume message" -> {
-                        addToParticipants(participantNames, it.to, participants, PARTICIPANT.called(it.to))
-                        addToParticipants(participantNames, it.from, participants, ENTITY.called(it.from))
-                    }
-                    else -> {
-                        addToParticipants(participantNames, it.from, participants, if (participants.isEmpty()) ACTOR.called(it.from) else PARTICIPANT.called(it.from))
-                        addToParticipants(participantNames, it.to, participants, PARTICIPANT.called(it.to))
-                    }
-                }
-            }
-        }
-        return participants
-    }
-
-    private fun addToParticipants(
-        participantNames: MutableSet<String>,
-        participantName: String,
-        participants: LinkedHashSet<Participant>,
-        participant: Participant
-    ) {
-        if (!participantNames.contains(participantName)) {
-            participants.add(participant)
-            participantNames.add(participantName)
-        }
     }
 }
---
+++ service/src/main/kotlin/com/lsdconsulting/generatorui/service/ParticipantListGenerator.kt
@@ -0,0 +1,49 @@
+package com.lsdconsulting.generatorui.service
+
+import com.lsd.ParticipantType
+import com.lsd.events.Message
+import com.lsd.events.SequenceEvent
+import com.lsd.events.SynchronousResponse
+import com.lsd.report.model.Participant
+import org.springframework.stereotype.Component
+
+@Component
+class ParticipantListGenerator {
+
+    fun generateParticipants(events: MutableList<SequenceEvent>): LinkedHashSet<Participant> {
+        val participantNames = mutableSetOf<String>()
+        val participants = linkedSetOf<Participant>()
+        events.forEach {
+            if (it is Message && it !is SynchronousResponse) {
+                when (it.label) {
+                    "publish event" -> {
+                        addToParticipants(participantNames, it.from, participants, ParticipantType.PARTICIPANT.called(it.from))
+                        addToParticipants(participantNames, it.to, participants, ParticipantType.ENTITY.called(it.to))
+                    }
+                    "consume message" -> {
+                        addToParticipants(participantNames, it.to, participants, ParticipantType.PARTICIPANT.called(it.to))
+                        addToParticipants(participantNames, it.from, participants, ParticipantType.ENTITY.called(it.from))
+                    }
+                    else -> {
+                        addToParticipants(participantNames, it.from, participants, if (participants.isEmpty()) ParticipantType.ACTOR.called(it.from) else ParticipantType.PARTICIPANT.called(it.from))
+                        addToParticipants(participantNames, it.to, participants, ParticipantType.PARTICIPANT.called(it.to))
+                    }
+                }
+            }
+        }
+        return participants
+    }
+
+    private fun addToParticipants(
+        participantNames: MutableSet<String>,
+        participantName: String,
+        participants: LinkedHashSet<Participant>,
+        participant: Participant
+    ) {
+        if (!participantNames.contains(participantName)) {
+            participants.add(participant)
+            participantNames.add(participantName)
+        }
+    }
+
+}
