530f964f9b6f56a96e2cdc840828be4e6ddd088b
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-04-24 21:46:57 +0100

    fix: small refactoring
    Use lsd context instead of directly using html report renderer to hide away details that might change

--- service/src/main/kotlin/com/lsdconsulting/generatorui/service/ParticipantListGenerator.kt
+++ service/src/main/kotlin/com/lsdconsulting/generatorui/service/ParticipantListGenerator.kt
@@ -1,10 +1,7 @@
 package com.lsdconsulting.generatorui.service
 
-import com.lsd.core.domain.Message
-import com.lsd.core.domain.MessageType
-import com.lsd.core.domain.Participant
+import com.lsd.core.domain.*
 import com.lsd.core.domain.ParticipantType.*
-import com.lsd.core.domain.SequenceEvent
 import org.springframework.stereotype.Component
 
 @Component
@@ -14,25 +11,28 @@ class ParticipantListGenerator {
         return linkedSetOf<Participant>().also { participants ->
             events.forEach {
                 if (it is Message && it.type != MessageType.SYNCHRONOUS_RESPONSE) {
+                    val fromName = it.from.componentName.normalisedName
+                    val toName = it.to.componentName.normalisedName
+                    
                     when (it.label.lowercase()) {
                         "publish event" -> {
-                            participants.add(PARTICIPANT.called(it.from.componentName.normalisedName, colour = "powderblue"))
-                            participants.add(ENTITY.called(it.to.componentName.normalisedName, colour = "green"))
+                            participants.add(PARTICIPANT.called(fromName, colour = "powderblue"))
+                            participants.add(ENTITY.called(toName, colour = "green"))
                         }
 
                         "consume message" -> {
-                            participants.add(PARTICIPANT.called(it.to.componentName.normalisedName, colour = "powderblue"))
-                            participants.add(ENTITY.called(it.from.componentName.normalisedName, colour = "green"))
+                            participants.add(PARTICIPANT.called(toName, colour = "powderblue"))
+                            participants.add(ENTITY.called(fromName, colour = "green"))
                         }
 
-                        else -> {
+                    else -> {
                             participants.add(
                                 if (participants.isEmpty())
-                                    ACTOR.called(it.from.componentName.normalisedName, colour = "orange")
+                                    ACTOR.called(fromName, colour = "orange")
                                 else
-                                    PARTICIPANT.called(it.from.componentName.normalisedName, colour = "powderblue")
+                                    PARTICIPANT.called(fromName, colour = "powderblue")
                             )
-                            participants.add(PARTICIPANT.called(it.to.componentName.normalisedName, colour = "powderblue"))
+                            participants.add(PARTICIPANT.called(toName, colour = "powderblue"))
                         }
                     }
                 }
--- service/src/test/kotlin/com/lsdconsulting/generatorui/service/LsdGeneratorShould.kt
+++ service/src/test/kotlin/com/lsdconsulting/generatorui/service/LsdGeneratorShould.kt
@@ -1,6 +1,5 @@
 package com.lsdconsulting.generatorui.service
 
-import com.lsd.core.IdGenerator
 import io.lsdconsulting.lsd.distributed.generator.diagram.InteractionGenerator
 import io.lsdconsulting.lsd.distributed.generator.diagram.dto.EventContainer
 import io.mockk.every
@@ -10,21 +9,19 @@ import org.junit.jupiter.api.Test
 import java.util.*
 
 class LsdGeneratorShould {
-    private val idGenerator = mockk<IdGenerator>(relaxed = true)
     private val interactionGenerator = mockk<InteractionGenerator>(relaxed = true)
     private val participantListGenerator = mockk<ParticipantListGenerator>(relaxed = true)
 
-    private val underTest: LsdGenerator = LsdGenerator(interactionGenerator, participantListGenerator, idGenerator)
+    private val underTest: LsdGenerator = LsdGenerator(interactionGenerator, participantListGenerator)
 
     @Test
     fun handleEmptyEventContainer() {
         val traceId = randomAlphanumeric(10)
-        every {idGenerator.next()} returns randomAlphanumeric(10)
         every { interactionGenerator.generate(mapOf(traceId to Optional.empty())) } returns EventContainer.builder()
             .events(
                 listOf()
             ).build()
-        every { participantListGenerator.generateParticipants(mutableListOf())} returns setOf()
+        every { participantListGenerator.generateParticipants(mutableListOf()) } returns setOf()
 
         underTest.captureInteractionsFromDatabase(traceId)
     }
