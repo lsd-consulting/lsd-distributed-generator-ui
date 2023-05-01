c8531572cb6e0ebaaabd95727732341a7fa26c97
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-05-01 23:06:29 +0100

    fix: bump to latest lsd-distributed-generator

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     }
 
     // LSD
-    implementation("io.github.lsd-consulting:lsd-distributed-generator:3.0.0")
+    implementation("io.github.lsd-consulting:lsd-distributed-generator:4.0.0")
 
     //////////////////////////////////
     // Unit test dependencies
